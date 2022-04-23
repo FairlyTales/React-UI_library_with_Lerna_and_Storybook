c42c4c8c4b3223832bdfcdf252cf78e4f4ce6c32
khnychkin_kirill <khnychkin_kirill@tutanota.com>
2022-04-23 18:10:31 +0300

    created a builder script using rollup and wired it to the components;

--- packages/builder/lib/builder.js
+++ packages/builder/lib/builder.js
@@ -1,1 +1,47 @@
-console.log("Lerna build is working");
+#!/usr/bin/env node
+const rollup = require('rollup');
+const path = require('path');
+const resolve = require('@rollup/plugin-node-resolve').default;
+const babel = require('@rollup/plugin-babel').default;
+
+const currentWorkingPath = process.cwd();
+const {main, name} = require(path.join(currentWorkingPath, 'package.json'));
+
+const inputPath = path.join(currentWorkingPath, main);
+
+const inputOptions = {
+	input: inputPath,
+	external: ['react'],
+	plugins: [
+		resolve(),
+		babel({
+			presets: ['@babel/preset-env', '@babel/preset-react'],
+			babelHelpers: 'bundled',
+		}),
+	],
+};
+
+const outputOptions = [
+	{
+		file: `dist/${name}.cjs.js`,
+		format: 'cjs',
+	},
+	{
+		file: `dist/${name}.esm.js`,
+		format: 'esm',
+	},
+];
+
+async function build() {
+	// create the bundle
+	const bundle = await rollup.rollup(inputOptions);
+
+	// create individual bundles
+	for (const options of outputOptions) {
+		await bundle.write(options);
+	}
+}
+
+build()
+	.then(() => console.log(`${name} build successful`))
+	.catch((err) => console.log(err));
